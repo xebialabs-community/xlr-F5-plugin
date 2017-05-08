d583bd737e72553feb4ee5403e450b68f800d88e
gsajwan <gauravsajwan30@gmail.com>
2017-05-08 10:40:15 -0400

    refractor

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # xlr-F5-plugin
 Plugin for GTM/LTM
+
+## Preface
+This document describes the functionality provide by the `xlr-F5-plugin`.
+
+## Overview
+#### GTM:
+Global traffic manager (GTM) integration with Xl release gives an option to enable and disable datacenters of choice. GTM gives you full control on the release flow.
+
+#### LTM:
+Local traffic manager (LTM) integration with Xl release gives an option to enable and disable pool-member in a specific pool whithout shutting down pool itself.
+It also gives you an option to enable/disable  multiple pool members.
+
+
+## Installation
+Copy the plugin JAR file into the `SERVER_HOME/plugins` directory of XL Release.
+Install Python 2.7.x and the additional [pycontrol](https://pypi.python.org/pypi/pycontrol) and [suds](https://pypi.python.org/pypi/suds) libraries on the xl release server.
+This plugin is mean to run the python script on the xl release windows server.
+
+### Configuring Template
+![enableLTM](images/enableLTM.png)
+
+
+![disableLTM](images/disableLTM.png)
+
+![enableGTM](images/enableGTM.png)
+
+![disableGTM](images/disableGTM.png)
+
+---
+## References:
+* https://devcentral.f5.com/wiki/iControl.LocalLB.ashx
+* https://devcentral.f5.com/wiki/iControl.GlobalLB.ashx
---
+++ src/main/resources/BigIP/enableGTMInF5.py
@@ -0,0 +1,158 @@
+# THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+# FOR A PARTICULAR PURPOSE. THIS CODE AND INFORMATION ARE NOT SUPPORTED BY XEBIALABS. 
+import sys
+import java.lang.System as System
+import java.text.SimpleDateFormat as Sdf
+import java.sql.Date as Date
+
+
+from java.lang import Exception
+from java.io import PrintWriter
+from java.io import StringWriter
+from java.lang import ClassLoader
+
+
+
+import os, re, sys, traceback
+
+import java.lang.String as String
+
+from com.xebialabs.overthere import CmdLine
+from com.xebialabs.overthere.util import CapturingOverthereExecutionOutputHandler, OverthereUtils
+from com.xebialabs.overthere.local import LocalConnection
+
+class WinLocalCmd():
+    def __init__(self, script):
+        self.script = script
+
+
+        self.stdout = CapturingOverthereExecutionOutputHandler.capturingHandler()
+        self.stderr = CapturingOverthereExecutionOutputHandler.capturingHandler()
+
+    def execute(self):
+
+        connection = None
+        try:
+            connection = LocalConnection.getLocalConnection()
+            tmp_workspace_file = connection.getTempFile('tmp_workspace')
+            workspace_path = re.sub('tmp_workspace', '', tmp_workspace_file.getPath())
+            workspace_directory = connection.getFile(workspace_path)
+            connection.setWorkingDirectory(workspace_directory)
+            new_path = "%s\gtm" % workspace_path
+            if not os.path.exists(new_path):
+                os.makedirs(new_path)
+            # connection.setWorkingDirectory(connection.getFile(self.scriptpath))
+            # upload the script and pass it to cscript.exe
+
+            gtm_enable_file = connection.getFile(OverthereUtils.constructPath(connection.getFile(new_path), 'gtm_enable.py'))
+            OverthereUtils.write(String(self.script).getBytes(), gtm_enable_file)
+            gtm_enable_file.setExecutable(True)
+            gtm_enable_file_path = gtm_enable_file.getPath()
+
+            targetFile = connection.getTempFile('gtm_enable_script', '.cmd')
+            batch_script =  "@echo off\r\ncd %s\r\npython %s \r\n" % (workspace_path, gtm_enable_file_path)
+            OverthereUtils.write(String(batch_script).getBytes(), targetFile)
+            targetFile.setExecutable(True)
+            # run cscript in batch mode
+            scriptCommand = CmdLine.build(targetFile.getPath())
+            return connection.execute(self.stdout, self.stderr, scriptCommand)
+        except Exception, e:
+            stacktrace = StringWriter()
+            writer = PrintWriter(stacktrace, True)
+            e.printStackTrace(writer)
+            self.stderr.handleLine(stacktrace.toString())
+            return 1
+        finally:
+            if connection is not None:
+                connection.close()
+
+    def getStdout(self):
+        return self.stdout.getOutput()
+
+    def getStdoutLines(self):
+        return self.stdout.getOutputLines()
+
+    def getStderr(self):
+        return self.stderr.getOutput()
+
+    def getStderrLines(self):
+        return self.stderr.getOutputLines()
+
+
+
+print username
+print password
+print address
+print connectionType
+
+
+scriptFile = """
+#!/usr/bin/python
+
+import pycontrol.pycontrol as pc
+import getpass
+import sys
+from sys import argv
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+bigip_address = '%s'
+bigip_user = '%s'
+bigip_pass = '%s'
+app_path='/%s/%s'
+data_center = '%s'
+
+print 'Connecting to BIG-IP at [' + bigip_address + '] as user [' + bigip_user + ']'
+bigip = pc.BIGIP(hostname = bigip_address, username = bigip_user, password = bigip_pass, fromurl = True, wsdls = ['GlobalLB.Application'])
+
+gtm_app  = bigip.GlobalLB.Application
+gtm_app_version  = gtm_app.get_version()
+print 'Detected version of data_center: ' + gtm_app_version
+data_centers = gtm_app.get_data_centers([app_path])
+print "Found the following data centers : {data_centers}".format(data_centers=str(data_centers))
+
+for dc in data_centers:
+    if isinstance(dc,list):
+        for dc_name in dc:
+            if data_center.lower() in dc_name.lower():
+                obj = gtm_app.typefactory.create('GlobalLB.Application.ApplicationContextObject')
+                obj.application_name = app_path
+                obj.object_name = dc_name
+                obj.object_type = gtm_app.typefactory.create('GlobalLB.Application.ApplicationObjectType').APPLICATION_OBJECT_TYPE_DATACENTER
+                print "Enabling Datacenter : {dc_name}".format(dc_name=dc_name)
+                try:
+                    gtm_app.enable_application_context_object([obj])
+                except Exception as e:
+                    print str(e)
+            else:
+                print "Skipping Datacenter : {dc_name}".format(dc_name=dc_name)
+                continue""" % ( bigIpAddress, bigIpUser, bigIpPass, bigActivePartitionName, bigApplicationName, bigAppDataCenter )
+
+
+
+
+
+script = WinLocalCmd(scriptFile)
+exitCode = script.execute()
+
+output = script.getStdout()
+err = script.getStderr()
+if (exitCode == 0):
+    print scriptFile
+    print "----"
+    print output
+else:
+    print "Exit code "
+    print exitCode
+    print
+    print "#### Output:"
+    print output
+
+    print "#### Error stream:"
+    print err
+    print
+    print "----"
+
+    #sys.exit(exitCode)
+    sys.exit(0)
